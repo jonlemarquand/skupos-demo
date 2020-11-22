3cd9b1c1741feb92b703097bb9a8a30b40a339e5
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-11-22 16:46:47 +0000

    Added get started grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,43 @@ const IndexPage = () => (
         <Link to="/" className="start-button-large">Get started</Link>
       </div>
     </div>
+    <div className="get-started-grid">
+      <div class="yellow gs-text">Hour set-up</div>
+      <div class="orange gs-text">No long-term commitments</div>
+      <div class="black gs-text">Skupos integrates with most popular POS systems and requires no long-term commitment, making it quick and easy to try with real customers today.</div>
+      <div class="purple gs-text">Remote setup</div>
+      <div class="mint gs-text">An internet connection</div>
+      <div class="skyblue gs-text">A Clover device or MNSP*</div>
+      <div class="gs-other">
+        Got an MNSP? <span class="fake-link">We're compatible.</span>
+      </div>
+    </div>
+    <div className="sell-smarter">
+      <h2>Everything you need to sell smarter</h2>
+      <p>Skupos simplifies every part of the process so all you have to do is run your store the same as before.</p>
+      <div className="sell-smarter-grid">
+        <div className="sell-smarter-grid-text">
+          <h3>Secure &amp; Reliable</h3>
+          <p>Skupos protects transaction data with bank-grade security.</p>
+        </div>
+        <div className="sell-smarter-img"></div>
+        <div className="sell-smarter-grid-text">
+          <h3>Easy to integrate</h3>
+          <p>Skupos protects transaction data with bank-grade security.</p>
+        </div>
+        <div className="sell-smarter-img"></div>
+        <div className="sell-smarter-grid-text">
+          <h3>Quick to get started</h3>
+          <p>Skupos protects transaction data with bank-grade security.</p>
+        </div>
+        <div className="sell-smarter-img"></div>
+        <div className="sell-smarter-grid-text">
+          <h3>Contract-free</h3>
+          <p>Skupos protects transaction data with bank-grade security.</p>
+        </div>
+        <div className="sell-smarter-img"></div>
+      </div>
+    </div>
     <div className="expert-support">
       <div className="expert-support-text">
         <h2>Expert support when you need it</h2>
