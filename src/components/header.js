e051ba8aaa6d31d1972e5039c685ecc1d1bc643a
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-11-22 11:27:04 +0000

    Initial setup

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,23 @@
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Logo from "../components/logo"
+
+import "./header.scss"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <ul className="left-header">
+      <li>Products</li>
+      <li>Resources</li>
+      <li>Support</li>
+    </ul>
+    <Logo />
+    <ul>
+      <li>Log in</li>
+      <li>Get started</li>
+    </ul>
   </header>
 )
 
